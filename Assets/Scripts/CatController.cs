cd175a8c433d891a820224a01b0ee3f5db1559ca
Lanuh <ianjurdak4@gmail.com>
2022-10-04 20:00:41 -0400

    Animation commit

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CatController : MonoBehaviour
 {
 
 public AudioClip musicClipOne;
@@ -11,12 +11,12 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
-
+Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,12 +26,14 @@ public AudioSource musicSource;
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
 
@@ -39,11 +41,13 @@ public AudioSource musicSource;
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
          }
 
